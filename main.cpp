9ac51f8cbe2dbdc9562ea1314e9f794aede2832c
j618p208 <joshjungpark@ku.edu>
2023-02-17 17:20:54 -0600

    The program works with the provided testing input but I realized that there is a problem with my code and have devised a new test input that exploits the problem

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@
 
 using namespace std;
 
-void print_array(string header, int array[5][5]) {
+void print_array(string header, int array[5][6]) {
     for (int i=0; i<5; i++) {
         cout << header << " " << i+1 << ": ";
+        for (int j=0; j<6; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print_array(string programmer, string department, int array[5][5]) {
+    for (int i=0; i<5; i++) {
+        cout << programmer << " " << i+1 << " wants " << department << ": ";
         for (int j=0; j<5; j++) {
             cout << array[i][j] << " ";
         }
@@ -14,9 +24,63 @@ void print_array(string header, int array[5][5]) {
     }
 }
 
+void rec_assign_programmers(int programmers[5][6], int departments[5][6]) {
+    int assign[5][5] = { //rows = programmer
+        {0,0,0,0,0},    //columns is the departments that want the programmer
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    };
+
+    for (int i=0; i<5; i++) {   //department
+        for (int j=0; j<5; j++) {   //programmer the department wants
+            if (departments[i][5] != 0) {   //if the department already has someone skip it
+                break;
+            }
+            if (programmers[departments[i][j]-1][5] != 0) {   //if the programmer is already assigned to a department skip them
+                continue;
+            }
+            for (int k=0; k<5; k++) {   //index in assign[][j]
+                if (assign[departments[i][j]-1][k] == 0) {
+                    assign[departments[i][j]-1][k] = i+1;    //adds the department i+1 to the programmer row it wants
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    for (int i=0; i<5; i++) {   //programmer in dupes
+        for (int j=0; j<5; j++) {   //department that the programmer wants
+            for (int k=0; k<5; k++) {   //departments that want them
+                if (programmers[i][j] == assign[i][k]) {    //if the department the programmer wants is the department that wants them
+                    programmers[i][5] = assign[i][k];       //assign them to each other
+                    departments[assign[i][k]-1][5] = i+1;
+                    break;
+                }
+            }
+            if (programmers[i][5] != 0) {
+                break;
+            }
+        }
+    }
+
+    bool finished = true;
+    for (int i=0; i<5; i++) {
+        if (programmers[i][5] == 0 || departments[i][5] == 0) {
+            finished = false;
+        }
+    }
+
+    if (!finished) {
+        rec_assign_programmers(programmers, departments);
+    }
+}
+
 int main() {
     ifstream infile;
-    infile.open("matching-data.txt");
+    infile.open("testing-data.txt");
 
     int departments[5][6];
     int programmers[5][6];
@@ -37,40 +101,14 @@ int main() {
 
     infile.close();
 
-    int temp_assign[5] = {0, 0, 0, 0, 0};   //Programmer i+1 will go to department temp_assign[i]
+    print_array("Programmers", programmers);
+    cout << endl;
+    print_array("Departments", departments);
 
-    int dupes[5][5] = { //rows = programmer
-        {0,0,0,0,0},    //columns is the departments that want the programmer
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0}
-    };
-
-    for (int i=0; i<5; i++) {   //department
-        for (int j=0; j<5; j++) {   //index in dupes[][j]
-            if (dupes[departments[i][0]-1][j] == 0) {
-                dupes[departments[i][0]-1][j] = i+1;    //adds the department i+1 to the programmer row it wants
-                break;
-            }
-        }
-    }
-
-    print_array("Programmer", dupes);
+    rec_assign_programmers(programmers, departments);
 
     for (int i=0; i<5; i++) {
-        for (int j=0; j<5; j++) {
-            if (dupes[i][j] == 0) {
-                if (j == 1) {
-                    temp_assign[i] = dupes[i][0];
-                }
-                break;
-            }
-        }
-    }
-
-    for (int i=0; i<5; i++) {
-        cout << temp_assign[i] << " ";
+        cout << "Department #" << i+1 << " will get Programmer #" << departments[i][5] << endl;
     }
 
     return 0;
